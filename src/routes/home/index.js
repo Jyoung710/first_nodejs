e2e55ff2a2d19b9bcbfdb91b0137294c19dee42a
Jyoung <youngyj0928@naver.com>
2024-03-13 02:25:47 +0900

    first commit

--- src/routes/home/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const ctrl = require('./home.ctrl');
-
-router.get('/', ctrl.hello);
-router.get('/login', ctrl.login);
-
-module.exports = router;
